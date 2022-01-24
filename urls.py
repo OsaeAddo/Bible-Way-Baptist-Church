e5983a6497c08aa4994f8376bfc2ffcb66b1906f
OsaeAddo <immanueloaddo@gmail.com>
2022-01-24 22:15:57 +0000

    modify urls.py

--- urls.py
+++ urls.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url, include
+from django.conf.urls import url, include, path
 from aldryn_django.utils import i18n_patterns
 import aldryn_addons.urls
 
 
 urlpatterns = [
+    path("", include("records_management.urls")),
     # add your own patterns here
 ] + aldryn_addons.urls.patterns() + i18n_patterns(
     # add your own i18n patterns here
