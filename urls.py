dbeb6eb75f28e0b7da2051b5264d35d36ba8d849
OsaeAddo <immanueloaddo@gmail.com>
2022-01-24 22:26:51 +0000

    modify urls.py

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url, include, path
+from django.conf.urls import url, include
+from django.urls import path
 from aldryn_django.utils import i18n_patterns
 import aldryn_addons.urls
 
