5b1c4ebc35858826b2af11ac324aace9ebd37c55
OsaeAddo <immanueloaddo@gmail.com>
2022-03-23 12:17:31 +0000

    add models

--- mainbank/models.py
+++ mainbank/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.forms import UUIDField
+import uuid
 
-# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    mobile = models.CharField(max_length=11, blank=True, null=True)
+    profile_pic = models.ImageField(upload_to='profile_pic/', null=True, blank=True, default="images/user.svg.png")
+    balance = models.FloatField(default=0, blank=True, null=True)
+    class Meta:
+        verbose_name_plural = "Customers"
+    
+    def __str__(self):
+        return f"{self.user.username}"
+
+
+class BankAccount(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    balance = models.FloatField(default=0)
+    account_number = models.UUIDField(primary_key=True, default=uuid.uuid4)
+
+
+    def __str__(self):
+        return f"{self.account_number}({self.customer.user.username})"
